d1c1d504336b08608cfe9530f93a8a0e0279493e
karanrn <karan4080@gmail.com>
2019-02-20 11:39:43 +0530

    Interactive Python in C# console.

---
+++ WithIronPython/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IronPython;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using Microsoft.Scripting.Hosting;
+
+namespace WithIronPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running python in C# application.");
+            ScriptEngine engine = Python.CreateEngine();
+            string inputText = String.Empty;            
+            while(true)
+            {
+                Console.Write(">>>");
+                inputText = Console.ReadLine();
+                if(inputText == "exit()")
+                    break;
+                var outputText = engine.Execute(inputText);
+                Console.WriteLine(outputText.ToString());
+            }
+            
+        }
+    }
+}
